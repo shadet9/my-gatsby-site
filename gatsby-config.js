0452953bfb108e796cc9a172e6b5345461391c84
shadet <shannonwcj@gmail.com>
2022-05-22 20:35:28 +0800

    finished part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
+    title: "my-gatsby-site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
