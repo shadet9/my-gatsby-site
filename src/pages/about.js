b357187dd23f0b23e8eeeb8f85d8f159098c1fbf
shadet <shannonwcj@gmail.com>
2022-05-22 14:12:52 +0800

    finished tutorial part II

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+//page
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there!I'm Ariel.I'm currently learning web development😆.</p>
+    </Layout>
+  );
+};
+
+//export
+export default AboutPage;
