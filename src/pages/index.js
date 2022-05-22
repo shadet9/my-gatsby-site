c761b4e5ee386a9824183e56ce4d67e6b03b935c
shadet <shannonwcj@gmail.com>
2022-05-22 19:01:42 +0800

    finished part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
+// import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 //style
 
@@ -10,6 +11,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/stamp.jpg"
+      />
     </Layout>
   );
 };
